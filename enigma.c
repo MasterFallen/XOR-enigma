a664855d98832b0383f566b2b87e91eca0e6f299
MasterFallen <mrfallen45@gmail.com>
2023-11-11 19:07:17 +0100

    23-11-11-breaking

--- enigma.c
+++ enigma.c
@@ -10,119 +10,121 @@
 /* (C) N. de Boer & A. Janikowska, 2023 */
 
 unsigned int lcg(int prevRandInt) {
-   return (A * prevRandInt + C) % M;
+  return (A * prevRandInt + C) % M;
 }
 
 unsigned int sg(unsigned char *listOfNums) {
-   unsigned int randNum = (listOfNums[29] - listOfNums[0]) % M;
-   // Shift the array to the left
-   for (int i = 0; i < 54; i++) {
-      listOfNums[i] = listOfNums[i + 1];
-   }
-   // Store the new random number at the end of the array
-   listOfNums[54] = lcg(listOfNums[53]);
-   return randNum;
-
+  unsigned int randNum = (listOfNums[29] - listOfNums[0]) % M;
+  // Shift the array to the left
+  for (int i = 0; i < 54; i++) {
+    listOfNums[i] = listOfNums[i + 1];
+  }
+  // Store the new random number at the end of the array
+  listOfNums[54] = lcg(listOfNums[53]);
+  return randNum;
 }
 
 void readFile(char *filename, char *text, int maxSize) {
-   FILE *file = fopen(filename, "rb");
-   if (file == NULL) {
-      printf("Failed to open the file.\n");
-      return;
-   }
+  FILE *file = fopen(filename, "rb");
+  if (file == NULL) {
+    printf("Failed to open the file.\n");
+    return;
+  }
 
-   fseek(file, 0, SEEK_END);
-   long fileSize = ftell(file);
-   fseek(file, 0, SEEK_SET);
+  fseek(file, 0, SEEK_END);
+  long fileSize = ftell(file);
+  fseek(file, 0, SEEK_SET);
 
-   if (fileSize >= maxSize) {
-      printf("File size exceeds the maximum limit.\n");
-      fclose(file);
-      return;
-   }
+  if (fileSize >= maxSize) {
+    printf("File size exceeds the maximum limit.\n");
+    fclose(file);
+    return;
+  }
 
-   fread(text, sizeof(char), fileSize, file);
-   fclose(file);
+  fread(text, sizeof(char), fileSize, file);
+  fclose(file);
 }
 
 int main(int argc, char *argv[]) {
-   char text[100];
-   int key = 0;
+  char text[100];
 
-   if (argc == 3 && strcmp(argv[1], "-f") == 0) {
-      /* Read text from file */
-      readFile(argv[2], text, sizeof(text));
-   } else {
-      /* Read text from user input */
-      printf("Please enter the text: ");
-      fgets(text, sizeof(text), stdin);
-   }
+  if (argc == 3 && strcmp(argv[1], "-f") == 0) {
+    /* Read text from file */
+    readFile(argv[2], text, sizeof(text));
+  } else {
+    /* Read text from user input */
+    printf("Please enter the text: ");
+    fgets(text, sizeof(text), stdin);
+  }
 
-   printf("Please enter the key: ");
-   scanf("%d", &key);
+  int encryptionMethod = 2;
+  printf("Please enter the desired encryption method: \n\n");
+  printf("[1] OS Random number generator, \n");
+  printf("[2] Linear congruential generator, \n");
+  printf("[3] Subtractive generator, \n");
+  scanf("%d", &encryptionMethod);
 
-   int encryptionMethod = 2;
-   printf("Please enter the desired encryption method: \n\n");
-   printf("[1] OS Random number generator, \n");
-   printf("[2] Linear congruential generator, \n");
-   printf("[3] Subtractive generator, \n");
-   scanf("%d", &encryptionMethod);
+  unsigned char key;
+  if (encryptionMethod == 1 || encryptionMethod == 2) {
+    printf("Please enter a 1-byte long key: ");
+    unsigned char key = 0;
+    scanf(" %c", &key);
+  } else if (encryptionMethod == 3) {
+    printf("Please enter a 55-byte long key: ");
+    unsigned char key[56];
+    for (int i = 0; i < 55; i++) {
+      scanf(" %c", &key[i]);
+    }
+    printf("First byte: %c", key[0]);
+    printf("Last byte: %c", key[54]);
+  } else {
+    printf("Wrong encryption method. Exiting...");
+    return 0;
+  }
 
-   unsigned int seed = key;
-   
-   /* If using Substractive generator, initialize it */
-   unsigned char subtractive[55];
-   if (encryptionMethod == 3) {
-      for (int i = 0; i < 55; i++) {
-         subtractive[i] = lcg(seed);
-         seed = subtractive[i];
-      }
-   }
+  int textLength = strlen(text);
+  unsigned char result[strlen(text)];
 
-   int textLength = strlen(text);
-   unsigned char result[strlen(text)];
+  for (int i = 0; i < strlen(text); i++) {
+    if (text[i] == '\n') {  // Avoid encrypting the end of input
+      result[i] = '\n';
+      continue;
+    }
+    /* Generate a byte based on different methods */
+    unsigned char randByte = 0;
+    switch (encryptionMethod) {
+      case 1:
+        srand(key);
+        randByte = rand() % 256;
+        key = randByte;
+        break;
+      case 2:
+        randByte = lcg(key);
+        key = randByte;
+        break;
+      case 3:
+        randByte = sg(&key);
+        break;
+      default:
+        break;
+    }
 
-   for (int i = 0; i < strlen(text); i++) {
-      if (text[i] == '\n') {  // Avoid encrypting the end of input
-         result[i] = '\n';
-         continue;
-      }
-      /* Generate a byte based on different methods */
-      unsigned char randByte = 0;
-      switch (encryptionMethod) {
-         case 1:
-            srand(seed);
-            randByte = rand() % 256;
-            seed = randByte; // Update seed
-            break;
-         case 2:
-            randByte = lcg(seed);
-            seed = randByte; // Update seed
-            break;
-         case 3:
-            randByte = sg(subtractive);
-            break;
-         default:
-            break;
-      }
+    result[i] = text[i] ^ randByte;  // XOR
+  }
+  result[textLength] = '\0';
+  printf("Output: %s", result);
 
-      result[i] = text[i] ^ randByte;  // XOR
-   }
-   result[textLength] = '\0';
-   printf("Output: %s", result);
+  /* Check wether we got a given file name. */
+  char *filename = argc == 3 ? argv[2] : "data.bin";
 
-   /* Check wether we got a given file name. */
-   char *filename = argc == 3 ? argv[2] : "data.bin";
+  FILE *file = fopen(filename, "wb");
+  if (file != NULL) {
+    fwrite(result, sizeof(unsigned char), textLength, file);
+    fclose(file);
+    printf("Result saved to %s\n", filename);
+  } else {
+    printf("Failed to open the file.\n");
+  }
 
-   FILE *file = fopen(filename, "wb");
-   if (file != NULL) {
-      fwrite(result, sizeof(unsigned char), textLength, file);
-      fclose(file);
-      printf("Result saved to %s\n", filename);
-   } else {
-      printf("Failed to open the file.\n");
-   }
-
-   return 0;
+  return 0;
 }
